d7be85868ae033090a5bd3203af88454b698de12
farhullin2017@gmail.com <90451035+IlnazFarkhullin@users.noreply.github.com>
2024-02-07 14:56:08 +0300

    рекурсия

---
+++ recursion_theory/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace recursion_theory
+{
+    internal class Program
+    {
+        //Пользователь вводит некое число.
+        //вычислить факториал числа с помощью рекурсивного метода
+        static int recursion(int a)// создаём метод с параметрром а
+        {
+            if (a == 0 || a == 1) //объявляем базовый случай
+            {
+                return 1;//возвращаем 1 если а равен нулю или единице
+
+            } 
+            else
+            {
+                return recursion(a-1) * a ;// вызываем сам метод для вычисления факториала
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Факториал числа {a} = {recursion(a)}"); //вызываем метод
+            Console.ReadKey();
+        }
+    }
+}
---
+++ recursion_theory_2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace recursion_theory_2
+{
+    internal class Program
+    {
+        //Фибоначчи
+        //Пользователь вводит число. Определить какое число в последовательности Фибоначи находится под номером который ввёл пользователь.
+        static  int Fibo(int n) // создаём метод с параметром n
+        {
+            if (n == 1) //Базовый варинт
+            {
+                return 1;
+            }
+            if (n == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return Fibo(n - 1) + Fibo(n - 2); //вычисление двух предыдущих чисел которое образует следующее
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число: ");
+            int n = Convert.ToInt32(Console.ReadLine()); //пользователь вводит число
+            Console.WriteLine(Fibo(n));//вызываем метод при выводе на консоль
+            Console.ReadKey();
+        }
+    }
+}
